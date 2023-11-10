d0dc0bf0461bbb7c132217869fb2645022fdef88
pranjali <pranjalisuskar@gmail.com>
2023-11-10 11:36:32 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Master from './website/component/Master';
+import All from './website/All';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Master Rcf={All}/>}></Route>
+  </Routes>
+  </BrowserRouter>
     </div>
   );
 }
---
+++ src/website/component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Footer
+        <h1>qwertyui</h1>
+    </div>
+  )
+}
+
+export default Footer
